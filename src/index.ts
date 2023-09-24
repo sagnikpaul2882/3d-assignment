21321b0b012ad32874ca19deeb25ad36b14dd95d
Sagnik Paul <sagnikpaul@Sagniks-MacBook-Pro.local>
2023-09-25 00:35:52 +0530

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,34 +5,66 @@ import Camera from './Camera'
 import Renderer from './Renderer'
 import Controls from './Controls'
 
-const scene = new Scene();
+class AssignmentObj {
+    scene: Scene
+    size: number
+    light: Light
+    camera: Camera
+    renderer: Renderer
+    controls: Controls
+    app: App
 
-const size = 266 // number of images
+    constructor() {
+        this.scene = new Scene();
+        this.size = 266;
+        this.light = new Light(0xffffff, 1000);
+        this.camera = new Camera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+        this.renderer = new Renderer();
+        this.controls = new Controls(this.camera.getCamera(), this.renderer.getRenderer().domElement);
+        this.app = new App(
+            this.scene.getScene(), 
+            this.size, 
+            this.light.getLight(), 
+            this.camera.getCamera(), 
+            this.renderer.getRenderer(), 
+            this.controls.getControls()
+        )
 
-const light = new Light(0xffffff, 1000);
-light.setPosition(2.5, 7.5, 15);
+        this.initializeObjects()
+        this.initializeEventListeners();
+    }
 
-const camera = new Camera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.setPosition(undefined, undefined, 10);
+    initializeObjects() {
+        this.light.setPosition(2.5, 7.5, 15);
+        this.camera.setPosition(undefined, undefined, 10);
+        this.renderer.setRendererSize(window.innerWidth, window.innerHeight);
+        this.controls.setDamping(true);
+    }
 
-const renderer = new Renderer();
-renderer.setRendererSize(window.innerWidth, window.innerHeight);
+    initializeEventListeners() {
+        let arrows = document.querySelectorAll('.arrows');
+        for (let i = 0; i < arrows.length; i++) {
+            arrows[i].addEventListener('click', () => this.app.onChangeIndex(i === 0 ? - 1 : 1), false)
+        }
 
-const controls = new Controls(camera.getCamera(), renderer.getRenderer().domElement);
-controls.setDamping(true);
-
-const app = new App(
-    scene.getScene(), 
-    size, 
-    light.getLight(), 
-    camera.getCamera(), 
-    renderer.getRenderer(), 
-    controls.getControls()
-)
-
-let arrows = document.querySelectorAll('.arrows');
-for (let i = 0; i < arrows.length; i++) {
-    arrows[i].addEventListener('click', () => app.onChangeIndex(i === 0 ? - 1 : 1), false)
+        window.addEventListener('resize', () => this.app.onResize(), false)
+    }
 }
 
-window.addEventListener('resize', () => app.onResize(), false)
+new AssignmentObj()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
